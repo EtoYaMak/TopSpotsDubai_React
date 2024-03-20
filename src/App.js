f068ef0053f44995a50873b72671a6251067c6dc
M A K <45846310+EtoYaMak@users.noreply.github.com>
2024-03-20 19:10:24 +0000

    vercel speed insights -

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Home from "./pages/Home";
 import Navbar from "./pages/components/Navbar";
 import Footer from "./pages/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
   return (
     <div className="App bg-white ">
       <Navbar />
       <Home />
       <Footer />
+      <SpeedInsights />
     </div>
   );
 }
